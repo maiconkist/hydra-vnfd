80f450bc72e63859f9551078241306236a189edd
Maicon Kist <maiconkist@gmail.com>
2018-12-05 19:43:34 +0000

    update

---
+++ osm.sh
@@ -0,0 +1,22 @@
+case "$1" in
+   "install")
+      osm nsd-delete hydra_aas_nsd
+      osm vnfd-delete hydra_aas_vnfd
+
+      osm vnfd-create hydra_aas_vnf.tar.gz
+      osm nsd-create  hydra_aas_ns.tar.gz
+   ;;
+   "list")
+      osm vnfd-list
+      osm nsd-list
+   ;;
+   "delete")
+      osm nsd-delete hydra_aas_nsd
+      osm vnfd-delete hydra_aas_vnfd
+   ;;
+   "ns-create")
+      osm ns-create --ns_name test --nsd_name hydra_aas_nsd --vim_account wins_5g
+   ;;
+esac
+
+
