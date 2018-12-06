e87f39b16300ed56ce9f9f833bb0812c18772c71
Maicon Kist <maiconkist@gmail.com>
2018-12-06 13:45:09 +0000

    update

--- osm.sh
+++ osm.sh
@@ -9,13 +9,17 @@ case "$1" in
    "list")
       osm vnfd-list
       osm nsd-list
+      osm ns-list
    ;;
    "delete")
       osm nsd-delete hydra_aas_nsd
       osm vnfd-delete hydra_aas_vnfd
    ;;
    "ns-create")
-      osm ns-create --ns_name test --nsd_name hydra_aas_nsd --vim_account wins_5g
+      osm ns-create --ns_name test --nsd_name hydra_aas_nsd --vim_account wins_5g --ssh_keys teste
+   ;;
+   "ns-delete")
+      osm ns-delete test
    ;;
 esac
 
